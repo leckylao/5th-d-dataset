62b58b2e5b59815b6cb5a93bb500ae48d4a6341d
Lecky Lao <leckylao@au1.ibm.com>
2014-07-13 07:03:40 +1000

    add in sinatra-contrib; add in graph;

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'httparty'
+require 'sinatra/json'
 
 AGENCIES = nil
 
@@ -18,12 +19,52 @@ get '/' do
     query += ".Out('#{params[:relationship3]}')" if params[:relationship3] && !params[:relationship3].empty?
     query += ".Out(['name', 'start date', 'end date']).All()"
     logger.info query
+    # fetch output
     @output = HTTParty.post(@action, {body: query})
     @output = JSON.parse(@output.body)["result"]
     logger.info @output.inspect
   end
   @relationships = %w(precedes subordinates)
   erb :index
+end
+
+get '/.json' do
+  @action = "http://agile-plains-1962.herokuapp.com/api/v1/query/gremlin"
+  @graph = "http://agile-plains-1962.herokuapp.com/api/v1/shape/gremlin"
+  if params[:agent] && params[:relationship1]
+    query = "g.V().Has('name', '#{params[:agent].gsub(/_/, ' ')}').Out('#{params[:relationship1]}')"
+    if params[:relationship2] && !params[:relationship2].empty?
+      query += ".Out('#{params[:relationship2]}')"
+      target_node = 6
+    end
+    if params[:relationship3] && !params[:relationship3].empty?
+      query += ".Out('#{params[:relationship3]}')" 
+      target_node = 8
+    else
+      target_node = 4
+    end
+    query = query + ".Out('name').All()"
+    logger.info query
+    @output = HTTParty.post(@action, {body: query})
+    @output = JSON.parse(@output.body)["result"]
+    logger.info @output.inspect
+    names = []
+    @output.each{|o| names << o["id"]}
+    names.uniq!
+    @graph = HTTParty.post(@graph, {body: query})
+    @graph = JSON.parse(@graph.body)
+    logger.info @graph.inspect
+    @graph["nodes"].each_with_index do |node, index|
+      if index == target_node && node["is_link_node"] == false
+        node["values"] = names
+      else
+        next
+      end
+    end
+    json @graph
+  else
+    "No result"
+  end
 end
 
 helpers do
